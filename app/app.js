a32b11aa4e6e807cd2b816bbd0eda29776d138e3
banales.antonio <38237083+Antonio-Banales@users.noreply.github.com>
2023-04-05 13:36:02 -0700

    initial git deposit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+const carousel = document.querySelector(".carousel");
+const slides = document.querySelectorAll(".slide");
+const controlLinks = document.querySelectorAll(".controls a");
+
+let 1 = 0,
+j = 1,
+intervalId;
+
+
